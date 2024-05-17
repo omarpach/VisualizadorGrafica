0b925c59b07102c8495689561bbef4ae465ff0c5
OmarP-hub <omy250@gmail.com>
2024-05-17 09:25:36 -0700

    Add: Hello World in main, Add: Resources dir
    - Hello World code in main.cpp
    - Resources directory with font file for text

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,34 @@
+#include "SFML/Graphics/Font.hpp"
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Graphics/Text.hpp"
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+int main() {
+  sf::RenderWindow window =
+      sf::RenderWindow{{1920u, 1080u}, "CMake SFML Project"};
+  window.setFramerateLimit(60);
+  std::string fuenteRuta = "resources/fonts/Arial.ttf";
+  sf::Font arial;
+  if (!arial.loadFromFile(fuenteRuta))
+    std::cerr << "Error al cargar la fuente" << std::endl;
+  sf::Text texto;
+  texto.setFont(arial);
+  texto.setStyle(sf::Text::Bold);
+  texto.setFillColor(sf::Color::Red);
+  texto.setString("Hola mundo");
+  texto.setCharacterSize(24);
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
+  while (window.isOpen()) {
+    for (auto event = sf::Event{}; window.pollEvent(event);) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
     }
+
+    window.clear();
+    window.draw(texto);
+    window.display();
+  }
 }
